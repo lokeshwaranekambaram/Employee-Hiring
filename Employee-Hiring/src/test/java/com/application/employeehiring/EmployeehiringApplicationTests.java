05a7fa9194d0a3d61c538455069ea2f60b73b55b
lokeshwaranekambaram <lokesh22905@gmail.com>
2024-10-20 09:29:03 +0530

    Docs: Initial Commit

---
+++ Employee-Hiring/src/test/java/com/application/employeehiring/EmployeehiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.employeehiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeehiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
