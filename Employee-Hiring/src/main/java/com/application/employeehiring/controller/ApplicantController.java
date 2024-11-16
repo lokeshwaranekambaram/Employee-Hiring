d02d8b2c6df4672db36632d943909c6592d61fc0
lokeshwaranekambaram <lokesh22905@gmail.com>
2024-11-16 17:21:08 +0530

    Feat : Update Controller,Service

--- Employee-Hiring/src/main/java/com/application/employeehiring/controller/ApplicantController.java
+++ Employee-Hiring/src/main/java/com/application/employeehiring/controller/ApplicantController.java
@@ -1,11 +1,27 @@
 package com.application.employeehiring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.application.employeehiring.model.Applicant;
+import com.application.employeehiring.service.ApplicantService;
+
 
 @RestController
 @RequestMapping("/Applicant")
 public class ApplicantController {
+     @Autowired
+          private ApplicantService applicantService;
+
+     @PostMapping("/addApplicant")
+     public String addApplicant(@RequestBody Applicant applicant) 
+     {
+          applicantService.addApplicant(applicant);
+          return "Applicant Added Successfully";
+     }
      
 
 }
--- Employee-Hiring/src/main/java/com/application/employeehiring/service/RecruiterService.java
+++ Employee-Hiring/src/main/java/com/application/employeehiring/service/RecruiterService.java
@@ -1,5 +1,31 @@
 package com.application.employeehiring.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.application.employeehiring.model.Recruiter;
+import com.application.employeehiring.repository.RecruiterRepo;
+
+@Service
 public class RecruiterService {
+     @Autowired
+     private RecruiterRepo recruiterRepo;
+
+     public Recruiter addRecruiter(Recruiter r)
+     {
+          return recruiterRepo.save(r);
+     }
+     public List<Recruiter>findAllRecruiter()
+     {
+          return recruiterRepo.findAll();
+     }
+     public Optional<Recruiter>getById(long id)
+     {
+          return recruiterRepo.findById(id);
+     }
+          
 
 }
